16d300529b1688d2e9a57c3431d38310c8032ab7
duda <118494747+rhadamantys666@users.noreply.github.com>
2024-08-26 15:48:07 -0300

    aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["duda", "vinicius", "neno", "karol", "douglas", "nana", "vesgo"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
